ab53d3f15d5cbe0c81183e1442f4379bcd25e2e6
Yuzux <61511020+Yuzux@users.noreply.github.com>
2020-11-05 17:05:17 +0100

    Add files via upload

--- Yuzux/index.php
+++ Yuzux/index.php
@@ -1,20 +1,60 @@
 <?php
-header('Content-Type: application/json');
 
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=api;', 'root', '');
-    $retour["success"] = true;
-    $retour["message"] = "Connexion reusit";
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exeption $e) {
-    $retour["success"] = false;
-    $retour["message"] = "Connexion BDD imposible";
+    die('Erreur : ' . $e->getMessage());
 }
 
-$requete = $pdo->prepare("SELECT * FROM Location");
-$requete->execute();
+$requete = $pdo->prepare("SELECT * FROM location WHERE id=1");
+$requete->execute(array());
+if($requete->rowCount() == 1)
+{
+    $location = $requete->fetch();
+    $lat = $location['Lat'];
+    $longi = $location['Longi'];
+}
+else 
+{
+    die('Non enregistrer');
+}
+?>
 
-$retour["success"] = true;
-$retour["message"] = "Localisation";
-$retour["results"] ["location"] = $requete->fetchAll();
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <font face="arial black">
+        <title>Test API</title>
+        </font>
 
-echo json_encode($retour);
+        <style>
+            #map{
+                height: 500px;
+                width: 800px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div id="map"></div>
+
+        <script>
+            function initMap(){
+                var location = {lat: <?= $lat?>, lng: <?= $longi?>};
+                var map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 12,
+                    center: location
+                });
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map:map
+                });
+            }
+        </script>
+        
+
+    </body>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRO-n4yCeHyLlM6PTx7RVSCjrWoA74Il4&callback=initMap"></script>
+</html>
